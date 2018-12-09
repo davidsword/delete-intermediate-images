9a74d1ac5ce2d30c4df53bf05dbf871a8267acd3
David Sword <david@davidsword.ca>
2018-12-09 06:14:14 -0800

    🔋 version number change

--- delete-thumbnails.php
+++ delete-thumbnails.php
@@ -3,13 +3,13 @@
  * Plugin Name:    Delete Thumbnails & Resized Images
  * Plugin URI:     https://davidsword.ca/wordpress-plugins/
  * Description:    Find and delete thumbnails & resized images from your Media Library
- * Version:        2.2.1
+ * Version:        2.3.0
  * Author:         davidsword
  * Author URI:     https://davidsword.ca/
  * License:        GPLv3
  * License URI:    https://www.gnu.org/licenses/quick-guide-gplv3.en.html
  * Text Domain:    dlthumbs
- * 
+ *
  * @package delete-thumbnails
  */
 
@@ -23,7 +23,7 @@ add_action( 'admin_init', function() {
  * Main and only Class for plugin.
  */
 class Delete_Thumbnails {
-	
+
 	/**
 	 * Class construct
 	 *
@@ -37,7 +37,7 @@ class Delete_Thumbnails {
 	 * Plugin initialization
 	 *
 	 * @TODO load_plugin_textdomain.
-	 * 
+	 *
 	 * @since 2.3.0
 	 * @return void
 	 */
@@ -54,7 +54,7 @@ class Delete_Thumbnails {
 	 * Hook the plugin into WordPress
 	 */
 	public function hooks() {
-		
+
 		// Add in our JS resources.
 		add_action( 'admin_enqueue_scripts', array( $this, 'admin_scripts' ) );
 
@@ -67,7 +67,7 @@ class Delete_Thumbnails {
 	 *
 	 * @TODO this all needs to be cached.
 	 * @TODO this only needs to be run on the options page.
-	 * 
+	 *
 	 * @return array of URLs of current media items.
 	 */
 	public function get_library() {
@@ -93,15 +93,15 @@ class Delete_Thumbnails {
 	 * @since 2.0
 	 */
 	public function add_admin_menu() {
-		$this->menu_id = add_management_page( 
-			__( 'Delete Thumbnails', 'dlthumbs' ), 
-			__( 'Delete Thumbnails', 'dlthumbs' ), 
+		$this->menu_id = add_management_page(
+			__( 'Delete Thumbnails', 'dlthumbs' ),
+			__( 'Delete Thumbnails', 'dlthumbs' ),
 			'administrator', // Caps level.
-			'dlthumbs', 
-			array( $this, 'interface' ) 
+			'dlthumbs',
+			array( $this, 'interface' )
 		);
 	}
-	
+
 	/**
 	 * Add Resources
 	 *
@@ -109,20 +109,20 @@ class Delete_Thumbnails {
 	 */
 	public function admin_scripts() {
 		if ( 'tools_page_dlthumbs' === get_current_screen()->base ) {
-			wp_register_style( 
-				'css', 
-				plugins_url( 'style.css', __FILE__ ), 
-				false, 
-				'2.0' 
+			wp_register_style(
+				'css',
+				plugins_url( 'style.css', __FILE__ ),
+				false,
+				'2.0'
 			);
 			wp_enqueue_style( 'css' );
 
-			wp_register_script( 
-				'js', 
-				plugins_url( 'dltumbs.js', __FILE__ ), 
-				array( 'jquery' ), 
-				'2.0', 
-				true 
+			wp_register_script(
+				'js',
+				plugins_url( 'dltumbs.js', __FILE__ ),
+				array( 'jquery' ),
+				'2.0',
+				true
 			);
 			wp_enqueue_script( 'js' );
 		}
@@ -146,27 +146,27 @@ class Delete_Thumbnails {
 	 */
 	public function interface() {
 		?>
-		<div class='wrap' id='dlthumbs'> 
+		<div class='wrap' id='dlthumbs'>
 			<h2><?php _e( 'Delete Resized Images', 'dlthumbs' ); ?></h2>
 
 			<?php
 			// Load files in media upload dir.
 			$this->files       = $this->get_files_from_folder( $this->dir );
 			$this->files_count = count( $this->files );
-			
+
 			// Check the dir for permissions.
 			$writable = $this->check_permission( $this->dir );
-			if ( ! $writable ) : 
+			if ( ! $writable ) :
 				?>
 				<div class='notice notice-error'><p>
 					<?php _e( 'This plugin requires Your upload directory CHMOD to be at least <code>755</code> so PHP can edit it. The deletion of files will most likely not work. Please contact your host or website provider for assistance. Mention your CHMOD is currently set to:', 'dlthumbs' ); ?> <code><?php echo $writable; ?></code>
 				</p></div>
-				<?php 
+				<?php
 			endif;
 
 			// Form submit, run deletion.
 			$this->dltthumbs_form_submit();
-			
+
 			// show thumbnails.
 			$this->dltthumbs_list_form();
 			?>
@@ -192,7 +192,7 @@ class Delete_Thumbnails {
 			return false;
 		}
 	}
-	
+
 	/**
 	 * Form Submit Actions
 	 *
@@ -200,8 +200,8 @@ class Delete_Thumbnails {
 	 */
 	public function dltthumbs_form_submit() {
 		if (
-			isset( $_POST['list'] ) && 
-			! empty( $_POST['list'] ) && 
+			isset( $_POST['list'] ) &&
+			! empty( $_POST['list'] ) &&
 			'[]' !== $_POST['list']
 		) {
 			if ( check_admin_referer( 'submit' ) ) {
@@ -224,13 +224,13 @@ class Delete_Thumbnails {
 				}
 				if ( count( $deleted ) > 0 ) {
 					$error_class = 'notice-success is-dismissible';
-					$error_text  = count( $deleted ); 
+					$error_text  = count( $deleted );
 					$error_text .= _e( 'files successfully deleted.', 'dlthumbs' ); // @TODO change to print_f format
 				}
 				if ( count( $not_deleted ) > 0 ) {
 					$error_class = 'notice-error';
 					$error_text  = _e( 'Yikes, files were marked to-delete but PHP was unable to delete them.', 'dlthumbs' ); // @TODO change to print_f format
-					$error_text .= count( $not_deleted ) . implode( '<br /> - ', $not_deleted ); 
+					$error_text .= count( $not_deleted ) . implode( '<br /> - ', $not_deleted );
 				}
 			} else {
 				$error_class = 'notice-error';
@@ -244,7 +244,7 @@ class Delete_Thumbnails {
 			<?php
 		}
 	}
-	 
+
 	/**
 	 * List all files from uploads directory.
 	 *
@@ -254,12 +254,12 @@ class Delete_Thumbnails {
 		?>
 		<div class="notice notice-<?php echo ( 0 === $this->files_count ) ? 'error' : 'info'; ?>">
 			<p>
-				<?php _e( 'Browsing', 'dlthumbs' ); ?>: 
-				<code>/<?php echo str_replace( get_home_path(), '', $this->dir ); ?>/</code> 
-				<?php echo $this->files_count; ?> 
-				<?php _e( 'files were found', 'dlthumbs' ); ?> 
-				<?php if ( $this->files_count > 0 ) { ?> 
-					, <span class='total_thumbnail_count'></span> 
+				<?php _e( 'Browsing', 'dlthumbs' ); ?>:
+				<code>/<?php echo str_replace( get_home_path(), '', $this->dir ); ?>/</code>
+				<?php echo $this->files_count; ?>
+				<?php _e( 'files were found', 'dlthumbs' ); ?>
+				<?php if ( $this->files_count > 0 ) { ?>
+					, <span class='total_thumbnail_count'></span>
 					<?php _e( 'images detected as resized images', 'dlthumbs' ); ?>.
 				<?php } ?>
 			</p>
@@ -280,7 +280,7 @@ class Delete_Thumbnails {
 				</tr>
 			</thead>
 			<tbody>
-				<?php 
+				<?php
 				$id = 0;
 				foreach ( $this->files as $afile ) {
 					$is_thumb = $this->is_thumbnail( $afile );
@@ -292,10 +292,10 @@ class Delete_Thumbnails {
 					?>
 					<tr>
 						<td>
-							&nbsp;<input id='input-<?php echo $id; ?>' type='checkbox' value='<?php echo str_replace( $this->dir, '', $file ); ?>' /> 
+							&nbsp;<input id='input-<?php echo $id; ?>' type='checkbox' value='<?php echo str_replace( $this->dir, '', $file ); ?>' />
 						</td>
 						<td>
-							<a target='_Blank' href='<?php echo $this->fixslash( str_replace( $this->dir, $this->url, $file ) ); ?>'>View »</a> 
+							<a target='_Blank' href='<?php echo $this->fixslash( str_replace( $this->dir, $this->url, $file ) ); ?>'>View »</a>
 						</td>
 						<td>
 							<label for='input-<?php echo $id; ?>'>
@@ -305,7 +305,7 @@ class Delete_Thumbnails {
 					</tr>
 					<?php
 				}
-				if ( 0 === $id || 0 === $this->files_count ) { 
+				if ( 0 === $id || 0 === $this->files_count ) {
 					?>
 					<tr>
 						<td colspan=3>
@@ -318,7 +318,7 @@ class Delete_Thumbnails {
 				}
 				?>
 			</tbody>
-		</table>	
+		</table>
 
 		<br />
 
@@ -327,24 +327,24 @@ class Delete_Thumbnails {
 			// security.
 			wp_nonce_field( 'submit' );
 
-			if ( 0 !== $id && 0 !== $this->files_count ) { 
+			if ( 0 !== $id && 0 !== $this->files_count ) {
 				?>
 
 			<!-- the magic -->
 			<textarea name='list'></textarea>
 			<p>
 				<label>
-					<input class='nag' value='' type='checkbox' name='nag1' /> 
+					<input class='nag' value='' type='checkbox' name='nag1' />
 					<?php _e( 'I understand that pressing the button below will delete the above selected files', 'dlthumbs' ); ?>.
 				</label>
 				<br />
 				<label>
-					<input class='nag' value='' type='checkbox' name='nag2' /> 
+					<input class='nag' value='' type='checkbox' name='nag2' />
 					<?php _e( 'I have backed up the uploads directory before doing this', 'dlthumbs' ); ?> (<code>/<?php echo str_replace( get_home_path(), '', $this->dir ); ?>/</code>).
 				</label>
 				<br />
 				<label>
-					<input class='nag' value='' type='checkbox' name='nag3' /> 
+					<input class='nag' value='' type='checkbox' name='nag3' />
 					<?php _e( 'I understand this action can not be undone', 'dlthumbs' ); ?>.</label><br />
 			</p>
 			<input type='submit' class='button-primary button-large button' value='<?php _e( 'DELETE RESIZED IMAGES', 'dlthumbs' ); ?> &raquo;' disabled>
@@ -354,7 +354,7 @@ class Delete_Thumbnails {
 		<p id='streetcred'><?php _e( 'Plugin By', 'dlthumbs' ); ?> <a href='https://davidsword.ca/' target='_Blank'>David Sword</a></p>
 		<?php
 	}
-	
+
 	/**
 	 * Function for indexing directory
 	 *
@@ -366,18 +366,18 @@ class Delete_Thumbnails {
 	 * @param string  $folder folder to retrive files from.
 	 * @param boolean $godeep true to look deeper than current dir, false if /.
 	 * @return array of files.
-	 */ 
+	 */
 	public function get_files_from_folder( $folder, $godeep = true ) {
 
 		$files = [];
-		
+
 		// start read.
 		if ( is_dir( $folder ) ) {
 			$dh       = opendir( $folder );
 			$filename = readdir( $dh );
 			while ( false !== $filename ) {
 				if ( ! in_array( $filename, $this->the_naughty_list(), true ) ) {
-	
+
 					// it's a dir, index contents w/ current function.
 					if ( $this->check_if_dir( $filename ) ) {
 						// repeat same function, find files within folders.
@@ -403,34 +403,34 @@ class Delete_Thumbnails {
 	public function the_naughty_list() {
 		return array( '.DS_Store', '.', '..', '' );
 	}
-	
+
 	/**
 	 * Determine if a file is a thumbnail or not
 	 *
 	 * @since 2.0
 	 * @param string $file file to find out if thumbnail or not.
 	 * @return boolean true if really a thumbnail, false if not.
-	 */ 
+	 */
 	public function is_thumbnail( $file ) {
 		/*
 		If it's in the media library as a main file, it's defnitantly not a thumbnail
-		It could of been mistaken as one if it's source was a downloaded thumbnail from 
+		It could of been mistaken as one if it's source was a downloaded thumbnail from
 		Another WordPress blog
 		*/
 		if ( in_array( str_replace( get_home_path(), '', $file ), $this->library, true ) ) {
 			return false;
 		}
-		
+
 		// If it has the thumbnail suffix, lets concider it.
 		preg_match( '"-([0-9]*x[0-9]*)."', $file, $matches );
 		if ( count( $matches ) > 0 ) {
 			return true;
 		}
-		
+
 		// Not sure what it is, just send it back as not a thumbnail.
 		return false;
 	}
-		
+
 	/**
 	 * Check if item is a dir or file
 	 *
@@ -439,7 +439,7 @@ class Delete_Thumbnails {
 	 * @since 2.0.0
 	 * @param string $filename the file name to check if it's a directory or not.
 	 * @return boolean true if is, false if now
-	 */ 
+	 */
 	public function check_if_dir( $filename ) {
 		$pos = strpos( $filename, '.' );
 		if ( false === $pos ) {
@@ -453,13 +453,13 @@ class Delete_Thumbnails {
 	 * Helper, replace double slash.
 	 *
 	 * @since 2.0.0
-	 * 
+	 *
 	 * @param string $str string to replace double slashes on.
 	 * @return string hopefully without double slashes.
-	 */ 
+	 */
 	public function fixslash( $str ) {
 		return str_replace( '//', '/', $str );
 	}
-	
+
 }
 ?>
