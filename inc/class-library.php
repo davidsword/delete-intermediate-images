9556abbadf744c552a4b0f9547e9301b3008d56e
David Sword <david@davidsword.ca>
2019-04-18 09:00:43 -0700

    👌 IMPROVE: optimize query & add cache

--- inc/class-library.php
+++ inc/class-library.php
@@ -14,27 +14,33 @@ class DL_Library {
 	/**
 	 * Get the Media Library
 	 *
-	 * @TODO this all needs to be cached.
-	 * @TODO this only needs to be run on the options page.
-	 * @TODO this should be in it's own class
-	 * @TODO should pageinate
-	 *
 	 * @return array of URLs of current media items.
 	 */
 	static function get() {
+
+		$result = wp_cache_get( 'dlthumbs_library' );
+		if ( true === $result ) {
+			return $result;
+		}
+
 		$library = [];
+
 		$args    = [
-			'post_type'      => 'attachment',
-			'post_mime_type' => 'image',
-			'numberposts'    => -1, // Watch out bigger sites, this might slow ya down.
-			'post_status'    => null,
-			'post_parent'    => null, // Any parent.
+			'suppress_filters'    => true,
+			'ignore_sticky_posts' => true,
+			'no_found_rows'       => false,
+			'post_type'           => 'attachment',
+			'post_status'         => 'publish',
+			'numberposts'         => -1, // Watch out bigger sites, this might slow ya down.
 		];
 
+		// @TODO this needs to be paginated at some point, it won't scale.
 		$attachments = get_posts( $args );
 		foreach ( $attachments as $post ) {
+			// @TODO filter out 'post_mime_type'      => 'image',
 			$library[] = DL_Helpers::fixslash( wp_get_attachment_url( $post->ID ) );
 		}
+		wp_cache_set( 'dlthumbs_library', $library );
 		return $library;
     }
 }
